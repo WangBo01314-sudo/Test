c64e3998f6ebe1b535df8df160b9f5d6a6f55ff1
WangBo01314 <a19910626889@sina.com>
2019-06-25 00:05:51 +0800

    使用idea测试

---
+++ src/com/itheima/Sh.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+/**
+ * @author ：
+ * @date ：Created in
+ * @description ：
+ * @version: 1.0
+ */
+public class Sh {
+    public static void main(String[] args) {
+        System.out.println("ss");
+    }
+}
